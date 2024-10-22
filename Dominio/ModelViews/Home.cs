7a86b650d8ccfa82ded23b6ca9671a82efdb9757
nanica-croma <florianapolespaula@gmail.com>
2024-10-22 13:16:05 +0100

    Atualizando!

---
+++ Dominio/ModelViews/Home.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace minimal_api.Dominio.ModelViews
+{
+    public struct Home
+    {
+        public string Mensagem { get => "Bem-vindo à API de Veículos - Minimal API!";}
+
+        public string Doc { get => "/swagger"; }
+    }
+}
--- Infraestruturas/Db/DbContexto.cs
+++ Infraestruturas/Db/DbContexto.cs
@@ -2,11 +2,57 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using minimal_api.Dominio.Entidades;
+using minimal_api.Entidades;
 
 namespace minimal_api.Infraestruturas.Db
 {
-    public class DbContexto
+    public class DbContexto : DbContext
     {
-        
+        private readonly IConfiguration _configuracaoAppSettings;
+
+        public DbContexto (IConfiguration configuracaoAppSettings)
+        {
+            _configuracaoAppSettings = configuracaoAppSettings;
+        }
+
+
+        public DbSet<Administrador> Administradores { get; set; } = default;
+
+        public DbSet<Veiculo> Veiculos { get; set;} = default;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Administrador>(). HasData(
+                new Administrador {
+                    Id = 1,
+                    Email = "Administrador@teste.com",
+                    Senha = "123456",
+                    Perfil = "Adm"
+                }
+            );
+        }
+
+        private void MasData(Administrador administrador)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+            var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql")?.ToString();
+            
+            if(!string.IsNullOrEmpty(stringConexao))
+            {
+                optionsBuilder.UseMySql(
+                    stringConexao,
+                    ServerVersion.AutoDetect(stringConexao)
+                );
+            }
+            }
+        }
     }
 }
